63cf3023703211a393e259c342d7e2ac59e536ef
Abhinav <abhinavsuman0204@gmail.com>
2023-09-10 19:06:58 +0200

    Code updated as of 10-09-2023 07:06 pm

--- 1.Mathematics.py
+++ 1.Mathematics.py
@@ -209,3 +209,76 @@ if __name__ == '__main__':
     print(f"LCM of {num1} and {num2} is {lcm(num1,num2)}") """
 
 
+## Check for prime number 
+# basic approach
+
+""" def isPrime(n):
+    if n == 1:
+        return False
+    
+    for i in range(2,n):
+        if n% i == 0:
+            return False
+    
+    return True
+
+if __name__ == '__main__':
+    n = int(input("Enter a number to check : "))
+    if isPrime(n):
+        print(f"{n} is a prime number !!")
+    else:
+        print(f"{n} is not a prime number !! ")
+ """
+
+## optimised approach -- checking if divisor pair is less than n  (i*i) <= n
+
+""" def isPrime(n):
+    if n == 1:
+        return False
+    
+    i = 2
+
+    while(i*i<=n):
+        if n%i == 0:
+            return False
+        i+=1
+
+    return True
+
+
+if __name__ == '__main__':
+    n = int(input("Enter a number to check : "))
+    if isPrime(n):
+        print(f"{n} is a prime number !!")
+    else:
+        print(f"{n} is not a prime number !! ") """
+
+##  super optimised approach 
+
+""" def isPrime(n):
+
+    if n == 1:
+        return False
+    
+    if n == 2 or n == 3:
+        return True
+    
+    if n%2 == 0  or n% 3 == 0:
+        return False
+    
+    i = 5
+    
+    while( i * i <= n):
+        if  n% i == 0 or n% (i+2) == 0 :
+            return False
+        
+        i+=6
+
+    return True
+
+if __name__ == '__main__':
+    n = int(input("Enter a number to check : "))
+    if isPrime(n):
+        print(f"{n} is a prime number !!")
+    else:
+        print(f"{n} is not a prime number !! ") """
